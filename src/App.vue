e4124e9ce606659aea45d56b3db5bc28fe26e78b
VueMid <turabovumid587@gmail.com>
2024-06-24 15:14:35 +0500

    First commit (06.24.2024/15:15)

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="overflow-x-hidden">
+    <RouterView />
+  </main>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style scoped></style>
